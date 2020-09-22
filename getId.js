37d78508467dbcd2ed6ae7e34da2a9506fc3738c
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-09-22 09:16:30 +0700

    Add files via upload

---
+++ getId.js
@@ -0,0 +1,9 @@
+function getID(){
+	var namadepan = document.getElementById('namadepan')
+	var namabelakang = document.getElementById('namabelakang')
+	var hobi = document.getElementById('hobi')
+
+	console.log(namadepan)
+	console.log(namabelakang)
+	console.log(hobi)
+}
